e0990550a537eb73b88a642810c90e20efc873fd
Robert Campbell <robert_campbell@abercrombie.com>
2015-12-10 19:23:45 -0500

    Few Major things 1) It now outputs information to the Screen 2) It shows the unique value for the Size and Colors 3) Added base level models, views, and templates 4) Created a ATB product which extends the product

--- backbone_product/template.js
+++ backbone_product/template.js
@@ -1,9 +1,22 @@
 ANFApp = ANFApp || {};
 ANFApp.template = ANFApp.template || {};
+
+ANFApp._global = ANFApp._global || {};
+ANFApp._global.template = ANFApp._global.template || {};
+
+
+// {
+// 		cid : <string>
+// 		name : <string>
+// 		selected : <boolean>
+// 		disabled : <boolean>
+// 		value : <string>
+// 		text : <string>
+// }
+ANFApp._global.template.Radio = "<label for='<%= cid %>'><input type='radio' name='<%= name%>' id='<%= cid %>' selected='<%= selected %>' disabled='<%= disabled %>' value='<%= value %>'/><%= text%></label>";
+
 ANFApp.template._product = ANFApp.template._product || {};
 
-
-
 ANFApp.template._product.Price = "<span class='list redline'><%= list.formated%></span><span class='offer'><%= offer.formated%>";
+ANFApp.template._product.Product = "<section><header><%= name %></header><img src='http://anf.scene7.com/is/image/anf/anf_<%= collection %>_<%= currentSelectedSeq %>_prod1'/><ul><li><%= shortDesc %></li><li class='product__price'></li><li class='product__seqs'></li><li class='product__sizes'></li></ul></section>";
 
-ANFApp.template._product.Price = "<div class='product__price'></div>";
--- backbone_product/view.js
+++ backbone_product/view.js
@@ -1,37 +1,128 @@
  	ANFApp = ANFApp || {};
+	ANFApp._global = ANFApp._global || {};
+
+	// Global Helper functions
+	ANFApp._global.View = Backbone.View.extend({
+		bindComponent : function(options) {
+    		var opt = {
+    			key : "",
+    			component : null
+    		};
+
+    		opt = _.extendOwn(opt, options);
+
+    		if (typeof opt.key === "string" && opt.key.trim().length >= 1 &&
+    			typeof component === "object" && component !== null && 
+    			typeof component.render === "function") {
+    			this.listenTo(this.model, "change:"+watch, component.render);
+    		}
+		},
+		bindComponents : function(optionsArray) {
+			if (typeof optionsArray === "object" && optionsArray !== null) {
+				if (!(optionsArray instanceof Array)) {
+					optionsArray = [optionsArray];
+				}
+
+				optionsArray.forEach(this.bindComponent, this);
+			}
+		}
+	});
+
+	ANFApp._global.View.Radio = ANFApp._global.View.extend({
+		template: _.template(ANFApp._global.template.Radio || ""),
+  //   	initialize: function(){
+		// },
+    	render: function() {
+			this.$el.html(this.template(_.extend({}, {cid: this.cid}, this.model.attributes))) ;
+			return this;
+		}
+	});
+
+	ANFApp._global.View.RadioList = ANFApp._global.View.extend({
+    	tagName: "div",
+    	className: "radio-list",
+    	// events: {},
+    	radioElements : [],
+    	initialize: function(){
+			this.render();
+			
+			this.radioElements = [];
+
+			this.collection.models.forEach(function(myLocalModel) {
+				var radioElement = new ANFApp._global.View.Radio({
+					model : myLocalModel 
+				});
+
+				radioElement.render().$el.appendTo(this.$el);
+
+			}, this);
+    	},
+    	render: function() {
+			return this;
+		}
+	});
+
 
     // Start : MODELS
 
     ANFApp.view = ANFApp.view || {};
     ANFApp.view._product = ANFApp.view._product || {};
 
-  	ANFApp.view._product.Price = Backbone.View.extend({
+
+
+
+
+  	ANFApp.view._product.Price = ANFApp._global.View.extend({
     	tagName: "div",
     	className: "product__price",
     	template: _.template(ANFApp.template._product.Price || ""),
     	// events: {},
-    	// initialize: function(){},
+		initialize: function() {
+			this.listenTo(this.model, "change", this.render);
+			this.render();
+	 	},
     	render: function() {
 			this.$el.html(this.template(this.model.attributes));
 			return this;
 		}
 	});
 
-	ANFApp.view._product.Product = Backbone.View.extend({
-    	el: "body",
-    	className: "product",
+	ANFApp.view._product.ATBProduct = ANFApp._global.View.extend({
+    	el: "#primary-content",
+    	//className: "product",
     	template: _.template(ANFApp.template._product.Product || ""),
     	// events: {},
-    	// initialize: function(){},
-    	render: function() {
+    	initialize: function(){
+			this.render();
 
-    		var productPrice = new ANFApp.view._product.Price({
-    			model: this.model.get('price'),
-    			el: this.$('.product__price')
+    		this.bindComponent({
+    			key : "currentPrice",
+    			component : new ANFApp.view._product.Price({
+	    			model: this.model.get('price'),
+	    			el: this.$('.product__price')
+	    		})
+    		});
+
+    		console.log(this.model.get('avaiableColorCollection'));
+
+    		this.bindComponent({
+    			key : "avaiableColorCollection",
+    			component : new ANFApp._global.View.RadioList({
+	    			collection: this.model.get('avaiableColorCollection'),
+	    			el: this.$('.product__seqs')
+	    		})
     		});
 			
-			productPrice.render();
-			
+			this.bindComponent({
+    			key : "avaiableSizeCollection",
+    			component : new ANFApp._global.View.RadioList({
+	    			collection: this.model.get('avaiableSizeCollection'),
+	    			el: this.$('.product__sizes')
+	    		})
+    		});
+
+    	},
+    	render: function() {
 			this.$el.html(this.template(this.model.attributes));
 			return this;
 		}
